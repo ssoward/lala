57608ea642a843c5bfc4ed08640a72d6a22bbcc1
ssoward <scott.soward@gmail.com>
2021-03-30 14:18:37 -0600

    test3

--- src/App.js
+++ src/App.js
@@ -9,14 +9,22 @@ function App() {
         <p>
           Favorite Talks
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <ul className="body">
+        <li><a className="App-link" target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1OfXXwQ1QAyHPor-VP7RWcRUvTzUphP6L/view?usp=sharing">Meaning of the Atonement - Cleon Skousen</a>      </li>
+        <li><a className="App-link" target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/12y7KeWwH51S2Qg3IvMqQhVcqnmqhVnBM/view?usp=sharing">Profile of a Prophet - Hugh B. Brown</a>          </li>
+        <li><a className="App-link" target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1U7BPlIq1F3tLhs8HOysfAJAFhJtHSc1p/view?usp=sharing">Last Testimony - Bruce R. McConkie</a>            </li>
+        <li><a className="App-link" target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1AdnQBj-GoxFn_Btw4NeoDaiOKUa8Dze1/view?usp=sharing">Last Testimony - David B. Haight</a>              </li>
+        <li><a className="App-link" target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1BYHRux_b20CJkUg-2SjA75d5pNCUlGiC/view?usp=sharing">Max Pinegar</a>                                   </li>
+        <li><a className="App-link" target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1_151qu-OSIaScZD-54ldKSU3nvjdOqLY/view?usp=sharing">Father Are You There - Hugh B. Brown</a>          </li>
+        <li><a className="App-link" target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1WVTeTu9M-hU4t2sH8TusdEEl50T0BwDG/view?usp=sharing">God is the Gardener - Hugh B. Brown</a>           </li>
+        <li><a className="App-link" target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1DW9OkIwHadCD6wi2shnc0Ops9lz69LhT/view?usp=sharing">Jack R. Christianson 1</a>                        </li>
+        <li><a className="App-link" target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1afYNQYDCfCd8UwUq8JyeA3yt1aWFWhjM/view?usp=sharing">Jack R. Christianson 2</a>                        </li>
+        <li><a className="App-link" target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1DbtWWv3OrOT2s2mu-6a8M6TN13srM_vF/view?usp=sharing">Milk and Meat of the Gospel - Cleon Skousen</a>   </li>
+        <li><a className="App-link" target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1Lz-nxA9DByzSkAxM2xK5BAscYwbgTTiR/view?usp=sharing">Jonah and the Whale - Cleon Skousen</a>           </li>
+        <li><a className="App-link" target="_blank" rel="noopener noreferrer" href="https://drive.google.com/drive/folders/1iGUFdIf3wnnSVD6PoNSVG0YiPzvxnurF?usp=sharing">EFY Music</a>                                   </li>
+        <li><a className="App-link" target="_blank" rel="noopener noreferrer" href="https://drive.google.com/drive/folders/16BR68RJWX70DXsm8eWGoiV_bK91SqVBu?usp=sharing">Joseph Smith - Truman G Madsen</a>              </li>
+
+    </ul>
       </header>
     </div>
   );
