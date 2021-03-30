5ee01bccac6730b1fdb7318a55377d006f2c1c23
ssoward <scott.soward@gmail.com>
2021-03-30 13:24:01 -0600

    Test.

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import img from './IMG_0004.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={img} className="App-logo" alt="lala" />
+        <p>
+          Favorite Talks
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
